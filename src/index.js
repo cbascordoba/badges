e084494d72d55fd3083d142abf47466a2f82c2e3
sebastian cordoba <cbascordoba14@gmail.com>
2019-12-17 15:31:40 -0500

    6. ReactDom.render obtiene dos argumentos el QUE y el DONDE si escribimos JSX es requisito importar React

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//el html a continuacion es JSX, javascript con html
+// y es gracias a la importacion de React
 const element = <h1>Hello, Platzi Badges!</h1>;
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
+//el que es el elemento html y el donde es el container
 ReactDOM.render(element, container);
