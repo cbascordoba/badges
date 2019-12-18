99e9e264796248a80ad03d7716584cb32ad48078
sebastian cordoba <cbascordoba14@gmail.com>
2019-12-18 11:58:31 -0500

    11. Aplicar estilos, localmente utilizando className en el componente e importandolo, utilizando bootstrap instalandolo con npm e importandolo o utilizando un css global e importandolo en index.js

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
+import "./styles/Badge.css"
 import confLogo from '../images/badge-header.svg'
 
 //los componentes son clases
 class Badge extends React.Component {
     //el metodo render define cual va a ser el resultado a mostrar en pantalla
     render() {
-        return <div>
-            <div>
+        return <div className="Badge">
+            <div className="Badge__header">
                 <img src={confLogo} alt="logo"></img>
             </div>
 
-            <div>
-                <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
+            <div className="Badge__section-name">
+                <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
                 <h1>Sebastian <br/>Cordoba</h1>
             </div>
-            <div>
-                <p>Frontend Engineer</p>
-                <p>@cbascordoba</p>
+            <div className="Badge__section-info">
+                <h3>Frontend Engineer</h3>
+                <div>@cbascordoba</div>
             </div>
-            <div>#react</div>
+            <div className="Badge__footer">#react</div>
         </div>;
     
     }
