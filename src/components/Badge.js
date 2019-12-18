b66473deb732318be58154d6f245bf2ee2206a51
sebastian cordoba <cbascordoba14@gmail.com>
2019-12-18 12:21:18 -0500

    12. Props, estos son argumentos de una funcion y en este caso serán los atributos de nuestro componente, estos props salen de una variable de la clase que se llama this.props y los valores son asignados directamente en el ReactDOM.render

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -7,6 +7,8 @@ import confLogo from '../images/badge-header.svg'
 class Badge extends React.Component {
     //el metodo render define cual va a ser el resultado a mostrar en pantalla
     render() {
+      
+      
         return <div className="Badge">
             <div className="Badge__header">
                 <img src={confLogo} alt="logo"></img>
@@ -14,11 +16,11 @@ class Badge extends React.Component {
 
             <div className="Badge__section-name">
                 <img className="Badge__avatar" src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar"></img>
-                <h1>Sebastian <br/>Cordoba</h1>
+                <h1>{this.props.firstName} <br/>{this.props.lastName}</h1>
             </div>
             <div className="Badge__section-info">
-                <h3>Frontend Engineer</h3>
-                <div>@cbascordoba</div>
+                <h3>{this.props.jobTitle}</h3>
+                <div>@{this.props.github}</div>
             </div>
             <div className="Badge__footer">#react</div>
         </div>;
--- src/index.js
+++ src/index.js
@@ -16,4 +16,12 @@ const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
 //el que es el elemento html y el donde es el container
-ReactDOM.render(< Badge />, container);
+
+//props son argumentos a una función
+//para este caso van a ser los atributos de nuestro componente
+ReactDOM.render(< Badge
+     firstName="Sebastián"
+     lastName="Córdoba"
+     jobTitle="Frontend Engineer"
+     github="cbascordoba"/>,
+      container);
